3f14d4a60c6aa1f633782c5798025139ab745e4e
BZerachBerman <116837011+BZerachBerman@users.noreply.github.com>
2025-09-17 20:58:11 -0400

    Put the CSS and JS in separate files

--- suits.js
+++ suits.js
@@ -1,121 +1,120 @@
 /*js for sticky nav*/
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
-
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
+    const nav = document.querySelector('.nav')
+    window.addEventListener('scroll', fixNav)
+    
+    function fixNav() {
+        if(window.scrollY > nav.offsetHeight + 150) {
+            nav.classList.add('active')
+        } else {
+            nav.classList.remove('active')
+        }
     }
-}
-// js for blurry
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
-
-let load = 0
-
-let int = setInterval(blurring, 20)
-
-function blurring() {
-  load++
-
-  if (load > 99) {
-    clearInterval(int)
-  }
-
-  loadText.innerText = `FLASH SALE! UP TO 60% OFF! `
-  loadText.style.opacity = scale(load, 0, 100, 1, )
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
-}
-
-// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
-
-// js for feedback
-const ratings = document.querySelectorAll('.rating')
-const ratingsContainer = document.querySelector('.ratings-container')
-const sendBtn = document.querySelector('#send')
-const panel = document.querySelector('#panel')
-let selectedRating = 'Satisfied'
-
-ratingsContainer.addEventListener('click', (e) => {
-    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.nextElementSibling.innerHTML
-    } else if(
-        e.target.parentNode.classList.contains('rating') &&
-        e.target.previousSibling &&
-        e.target.previousElementSibling.nodeName === 'IMG'
-    ) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.innerHTML
+    // js for blurry
+    const loadText = document.querySelector('.loading-text')
+    const bg = document.querySelector('.bg')
+    
+    let load = 0
+    
+    let int = setInterval(blurring, 20)
+    
+    function blurring() {
+      load++
+    
+      if (load > 99) {
+        clearInterval(int)
+      }
+    
+      loadText.innerText = `FLASH SALE! UP TO 60% OFF! `
+      loadText.style.opacity = scale(load, 0, 100, 1, )
+      bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
     }
-
-})
-
-sendBtn.addEventListener('click', (e) => {
-    panel.innerHTML = `
-        <i class="fas fa-heart"></i>
-        <strong>Thank You!</strong>
-        <br>
-        <strong>Feedback: ${selectedRating}</strong>
-        <p>We'll use your feedback to improve our customer support</p>
-    `
-})
-
-function removeActive() {
-    for(let i = 0; i < ratings.length; i++) {
-        ratings[i].classList.remove('active')
+    
+    // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+      return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
     }
-}
-
-
-// js for rotating images
-
-const imgs = document.getElementById('imgs')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
-
-const img = document.querySelectorAll('#imgs img')
-
-let idx = 0
-
-let interval = setInterval(run, 2000)
-
-function run() {
-    idx++
-    changeImage()
-}
-
-function changeImage() {
-    if(idx > img.length - 1) {
-        idx = 0
-    } else if(idx < 0) {
-        idx = img.length - 1
+    
+    // js for feedback
+    const ratings = document.querySelectorAll('.rating')
+    const ratingsContainer = document.querySelector('.ratings-container')
+    const sendBtn = document.querySelector('#send')
+    const panel = document.querySelector('#panel')
+    let selectedRating = 'Satisfied'
+    
+    ratingsContainer.addEventListener('click', (e) => {
+        if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+            removeActive()
+            e.target.parentNode.classList.add('active')
+            selectedRating = e.target.nextElementSibling.innerHTML
+        } else if(
+            e.target.parentNode.classList.contains('rating') &&
+            e.target.previousSibling &&
+            e.target.previousElementSibling.nodeName === 'IMG'
+        ) {
+            removeActive()
+            e.target.parentNode.classList.add('active')
+            selectedRating = e.target.innerHTML
+        }
+    
+    })
+    
+    sendBtn.addEventListener('click', (e) => {
+        panel.innerHTML = `
+            <i class="fas fa-heart"></i>
+            <strong>Thank You!</strong>
+            <br>
+            <strong>Feedback: ${selectedRating}</strong>
+            <p>We'll use your feedback to improve our customer support</p>
+        `
+    })
+    
+    function removeActive() {
+        for(let i = 0; i < ratings.length; i++) {
+            ratings[i].classList.remove('active')
+        }
     }
-
-    imgs.style.transform = `translateX(${-idx * 500}px)`
-}
-
-function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000)
-}
-
-rightBtn.addEventListener('click', () => {
-    idx++
-    changeImage()
-    resetInterval()
-})
-
-leftBtn.addEventListener('click', () => {
-    idx--
-    changeImage()
-    resetInterval()
-})
-app.use(express.static( "pics" ));
+    
+    
+    // js for rotating images
+    
+    const imgs = document.getElementById('imgs')
+    const leftBtn = document.getElementById('left')
+    const rightBtn = document.getElementById('right')
+    
+    const img = document.querySelectorAll('#imgs img')
+    
+    let idx = 0
+    
+    let interval = setInterval(run, 2000)
+    
+    function run() {
+        idx++
+        changeImage()
+    }
+    
+    function changeImage() {
+        if(idx > img.length - 1) {
+            idx = 0
+        } else if(idx < 0) {
+            idx = img.length - 1
+        }
+    
+        imgs.style.transform = `translateX(${-idx * 500}px)`
+    }
+    
+    function resetInterval() {
+        clearInterval(interval)
+        interval = setInterval(run, 2000)
+    }
+    
+    rightBtn.addEventListener('click', () => {
+        idx++
+        changeImage()
+        resetInterval()
+    })
+    
+    leftBtn.addEventListener('click', () => {
+        idx--
+        changeImage()
+        resetInterval()
+    })
